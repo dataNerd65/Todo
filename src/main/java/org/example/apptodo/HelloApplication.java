c96e8c32d6636ee99cf5abe707199271486938a6
dataNerd65 <pkiarie811@gmail.com>
2024-06-21 21:52:18 +0300

    Starting out with a cool todo list application

--- src/main/java/org/example/apptodo/HelloApplication.java
+++ src/main/java/org/example/apptodo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 680);
+        Scene scene = new Scene(fxmlLoader.load(), 850, 720);
 
         //Load the CSS file
         String css = HelloApplication.class.getResource("styles.css").toExternalForm();
