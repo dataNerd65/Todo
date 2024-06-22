5413d2f24b6b97a72b673186db08d548bf33ab7d
dataNerd65 <pkiarie811@gmail.com>
2024-06-22 12:25:19 +0300

    Working on the signup logic

--- src/main/java/org/example/apptodo/HelloController.java
+++ src/main/java/org/example/apptodo/HelloController.java
@@ -1,10 +1,13 @@
 package org.example.apptodo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+
 
 
 public class HelloController {
@@ -30,6 +33,14 @@ public class HelloController {
     private Button signupButton;
     @FXML
     private Button clearButton1;
+
+    public void showAlert(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
     @FXML
     public void handleClearButtonAction(){
         System.out.println("Cancel Button clicked!");
@@ -47,6 +58,20 @@ public class HelloController {
     }
     @FXML
     public void  handleSignUpButtonAction(){
+        System.out.println("SignUp button clicked!");
+        String username = username1.getText();
+        String password = password2.getText();
+        String confirmPassword = password3.getText();
+        String email = this.email.getText();
+        String fullName = Fname.getText();
+
+        //validating the user input
+        if(username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty() || fullName.isEmpty()) {
+            showAlert("Error", "Invalid Input", "All fields must be filled out!");
+        }
+        if(password != confirmPassword) {
+            showAlert("Error", "Invalid Input" , "Passwords do not match!");
+        }
 
     }
     @FXML
