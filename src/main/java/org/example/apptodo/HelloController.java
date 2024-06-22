5d749d73a727994d44be1e86c13e4ca07226e54d
dataNerd65 <pkiarie811@gmail.com>
2024-06-22 11:14:01 +0300

    Handling the controls of the buttons

--- src/main/java/org/example/apptodo/HelloController.java
+++ src/main/java/org/example/apptodo/HelloController.java
@@ -1,8 +1,59 @@
 package org.example.apptodo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
+    @FXML
+    public TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField Fname;
+    @FXML
+    private TextField username1;
+    @FXML
+    private PasswordField password2;
+    @FXML
+    private PasswordField password3;
+    @FXML
+    private Button signupButton;
+    @FXML
+    private Button cancel1;
+    @FXML
+    public void handleCancelButtonAction(){
+        System.out.println("Cancel Button clicked!");
+        username.clear();
+        password.clear();
+    }
+    @FXML
+    public void handleLoginButtonAction(){
+        if (username.getText().isEmpty() || password.getText().isEmpty()){
+            System.out.println("Username or password cannot be empty!");
+        }else{
+            System.out.println("Username: " + username.getText() + " ,Password: " +password.getText());
+        }
+
+    }
+    @FXML
+    public void  handleSignUpButtonAction(){
+
+    }
+    @FXML
+    public void handleCancel1ButtonAction(){
+        System.out.println("Cancel Button Clicked!");
+
+    }
 
 }
+
